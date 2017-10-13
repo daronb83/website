90ec25704ffc5d0551dada9dc6d3f0aa5220f5a1
csweb2 <webmaster@cs.byu.edu>
2017-10-13 12:55:04 -0600

    news

--- angular-routing.js
+++ angular-routing.js
@@ -1,69 +1,92 @@
-angular.module('News', ['ui.router'])
-.config([
-'$stateProvider',
-'$urlRouterProvider',
-function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-    .state('home', {
-      url: '/home',
-      templateUrl: '/home.html',
-      controller: 'MainCtrl'
-    })
-    .state('posts', {
+// App
+var newsApp = angular.module('News', ['ui.router'])
+
+// Configuration
+var config = newsApp.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+
+  function($stateProvider, $urlRouterProvider) {
+    var home = $stateProvider.state('home', {
+        url: '/home',
+        templateUrl: '/home.html',
+        controller: 'MainCtrl'
+    });
+
+    home.state('posts', {
       url: '/posts/{id}',
       templateUrl: '/posts.html',
       controller: 'PostsCtrl'
     });
 
-  $urlRouterProvider.otherwise('home');
-}])
-.factory('postFactory', [function(){
-  var o = {
-    posts: []
-  };
-  return o;
-}])
-.controller('MainCtrl', [
-'$scope',
-'postFactory',
-function($scope, postFactory){
-  $scope.test = 'Hello world!';
+    $urlRouterProvider.otherwise('home');
+  }
+]);
 
-  $scope.posts = postFactory.posts;
+// Factory
+var factory = newsApp.factory('postFactory', [
+  function() {
+    var o = {
+      posts: []
+    };
 
-  $scope.addPost = function(){
-    if($scope.formContent === '') { return; }
-    $scope.posts.push({
-      title: $scope.formContent,
-      upvotes: 0,
-      comments: [
-      ]
-    });
-    $scope.formContent = '';
-  };
+    return o;
+  }
+]);
 
-  $scope.incrementUpvotes = function(post) {
-    post.upvotes += 1;
-  };
+// Main controller
+var mainCtrl = newsApp.controller('MainCtrl', [
+  '$scope',
+  'postFactory',
 
-}])
-.controller('PostsCtrl', [
-'$scope',
-'$stateParams',
-'postFactory',
-function($scope, $stateParams, postFactory){
-  $scope.post = postFactory.posts[$stateParams.id];
+  function($scope, postFactory) {
+    $scope.test = 'Hello world!';
+    $scope.posts = postFactory.posts;
 
-  $scope.addComment = function(){
-    if($scope.body === '') { return; }
-    $scope.post.comments.push({
-      body: $scope.body,
-      upvotes: 0
-    });
-    $scope.body = '';
-  };
+    $scope.addPost = function(){
+      if($scope.formContent === '') {
+        return;
+      }
 
-  $scope.incrementUpvotes = function(comment){
-    comment.upvotes += 1;
-  };
-}]);
+      $scope.posts.push({
+        title: $scope.formContent,
+        upvotes: 0,
+        comments: []
+      });
+
+      $scope.formContent = '';
+    };
+
+    $scope.incrementUpvotes = function(post) {
+      post.upvotes += 1;
+    };
+  }
+]);
+
+// Posts controller
+var postsCtrl = newsApp.controller('PostsCtrl', [
+  '$scope',
+  '$stateParams',
+  'postFactory',
+
+  function($scope, $stateParams, postFactory) {
+    $scope.post = postFactory.posts[$stateParams.id];
+
+    $scope.addComment = function(){
+      if($scope.body === '') {
+        return;
+      }
+
+      $scope.post.comments.push({
+        body: $scope.body,
+        upvotes: 0
+      });
+
+      $scope.body = '';
+    };
+
+    $scope.incrementUpvotes = function(comment){
+      comment.upvotes += 1;
+    };
+  }
+]);
